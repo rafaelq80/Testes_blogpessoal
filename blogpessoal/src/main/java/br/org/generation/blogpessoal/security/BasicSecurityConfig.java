b9584b2ff67c3d6626c851e8fc2649f5ac5d1339
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2021-07-13 01:07:39 -0300

    Update BasicSecurityConfig.java

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -20,13 +20,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-		
-		auth.inMemoryAuthentication()
-		.withUser("root")
-		.password(passwordEncoder().encode("root"))
-		.authorities("ROLE_USER");
-	
+		auth.userDetailsService(userDetailsService);	
 	}
 
 	@Bean

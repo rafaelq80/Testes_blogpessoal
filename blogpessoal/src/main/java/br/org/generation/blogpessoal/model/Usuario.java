29ac00796187c79d01b156e7eac9e98e53114731
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2021-07-13 02:53:10 -0300

    Update Usuario.java

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -26,21 +26,22 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
-	@Size(min = 5, max = 100)
+	@NotNull(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String nome;
 	
-	@NotNull
-	@Email
+	@NotNull(message = "O atributo usuário é obrigatório")
+	@NotBlank(message = "O atributo usuário não pode ser vazio")
+	@Email(message = "O atributo usuário deve ser um email")
 	private String usuario;
 	
-	@NotNull
-	@Size(min = 8)
+	@NotNull(message = "O atributo senha é obrigatório")
+	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
 	private String senha;
 	
 	@Column(name = "dt_nascimento")
 	@JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate dataNascimento;
+    	private LocalDate dataNascimento;
 	
 	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
